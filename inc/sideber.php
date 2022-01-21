513720ee7fbe17079d691a9369fc02f068cc4fe6
ashikishere <azashikishere@gmail.com>
2022-01-21 14:16:20 +0600

    search btn added

--- inc/sideber.php
+++ inc/sideber.php
@@ -11,7 +11,8 @@ $post              = $homeObj->display_recent_post();
         <div class="col-lg-12">
             <div class="sidebar-item search">
                 <form id="search_form" name="gs" method="GET" action="#">
-                    <input type="text" name="q" class="searchText" placeholder="type to search..." autocomplete="on">
+                    <input type="text" name="q" class="searchText" placeholder="type to search..." autocomplete="off">
+                    <input type="submit" value="Search" class="btn btn-success mt-2" style="color: #fff;">
                 </form>
             </div>
         </div>
